9f41f4345d778a65f1164bf931a0e44231a770b6
“Jose <Gutierrez@Gutierrezs-MacBook-Air.local>
2017-05-10 21:28:21 -0500

    added new files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170502013832) do
+ActiveRecord::Schema.define(version: 20170511020116) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,8 @@ ActiveRecord::Schema.define(version: 20170502013832) do
     t.integer  "phone_number"
     t.datetime "created_at",   null: false
     t.datetime "updated_at",   null: false
+    t.string   "middle_name"
+    t.text     "bio"
   end
 
 end
